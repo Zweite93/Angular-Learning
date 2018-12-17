40fd971e4548ff68badd3f962c3eb8a22938e8a1
Zweite93 <zweite93@gmail.com>
2018-12-17 19:08:59 +0200

    Added authentication guard service.

--- Angular Test App/angular5-app/src/app/app-routing.modules.ts
+++ Angular Test App/angular5-app/src/app/app-routing.modules.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { AuthGuard } from './helpers/canActivateAuthGuard';
+import { AuthGuardService as AuthGuard } from './services/auth-guard.service';
 import { LoginComponent } from './components/login/login.component';
 import { LogoutComponent } from './components/login/logout.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
